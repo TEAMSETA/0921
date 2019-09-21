f64974d46c3c384bce45dcd525bf363cd61c759f
TEAMSETA <42888355+TEAMSETA@users.noreply.github.com>
2019-09-21 12:09:16 +0900

    Add files via upload

---
+++ exam01.py
@@ -0,0 +1,6 @@
+kr = 80
+en = 75
+math = 55
+count = kr + en + math
+avr = count / 3
+print("홍길동 학생의 평균은 {} 입니다.".format(avr))
---
+++ exam02.py
@@ -0,0 +1,7 @@
+a = 13
+#a = int(input("숫자 입력:"))
+
+if a % 2 == 0:
+    print("{}는 짝수입니다.".format(a))
+else:
+    print("{}는 홀수입니다.".format(a))
